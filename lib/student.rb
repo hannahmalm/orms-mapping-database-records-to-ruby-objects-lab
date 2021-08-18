0efe7c92bc2cc7dedcc89b8068296192b6539fab
Hannah Malm <hannahemalm616@gmail.com>
2021-08-18 09:10:52 -0400

    commit tree 8c4d19f78595ccf444c46a9a216c6e3212f3ee4a

--- lib/student.rb
+++ lib/student.rb
@@ -3,19 +3,43 @@ class Student
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
+    student = self.new
+    #get the students id which will be the first in the array
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row[2]
   end
 
   def self.all
-    # retrieve all the rows from the "Students" database
+    # retrieve all the rows from the "Students" database using the wildcard
     # remember each row should be a new instance of the Student class
+    #the DB con and the loop will be the same for every instance
+    sql = <<- SQL
+      SELECT * 
+      FROM students
+    SQL 
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end 
   end
 
   def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
+    #when you dont know the name use a ? 
+    #to only find one record limit 1 and end.first
+    sql = <<- SQL 
+      SELECT *
+      FROM students
+      WHERE name = ? 
+      LIMIT 1
+    SQL 
+
+    DB[:conn].execute(sql,name).map do |row|
+      self.new_from_db(row)
+    end.first
   end
   
   def save
+    #this is the standard save
     sql = <<-SQL
       INSERT INTO students (name, grade) 
       VALUES (?, ?)
@@ -25,6 +49,7 @@ class Student
   end
   
   def self.create_table
+    #check to see if table exists first, otherwise make it and connect to db
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
       id INTEGER PRIMARY KEY,
